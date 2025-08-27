ba8f69dd308c9aba0175a7a551f339a42e05e4da
deveshsoni7 <devesh.soniwork@gmail.com>
2025-08-28 01:49:50 +0530

    User model ko complete kiyaa hai with access and refreshToken

--- weather-dashboard/server/models/UserModel.js
+++ weather-dashboard/server/models/UserModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 const userSchema = new mongoose.Schema({
 
     username:{
@@ -20,7 +20,52 @@ const userSchema = new mongoose.Schema({
 
 
 })
+//password ko bycrypt kiya hai 
+userSchema.pre("save",async function (next) {
+    if(!this.Modified("password")) return next();
+    this.password = bcrypt.hash(this.password,10);
+    next();
 
-const User = mongoose.model("User",userSchema);
+})
 
-export default User;
+//methods bhi add kr skte hai 
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+
+    return await bcrypt.compare(password,this.password)
+    
+}
+
+//jwt
+
+userSchema.methods.generateAccessToken = function(){
+
+    return jwt.sign({
+        _id : this._id,
+        email: this.email,
+        username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+
+}
+userSchema.methods.generateRefreshToken = function(){
+
+    jwt.sign({
+        _id:this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+
+}
+
+
+
+
+export const User = mongoose.model("User",userSchema);
